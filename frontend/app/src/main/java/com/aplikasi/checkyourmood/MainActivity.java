41e5833f04bbfceb5c4c45261784a9243eace240
Yudhistira <yudhistira30122008@gmail.com>
2026-02-08 17:04:36 +0700

    feat: initial commit

---
+++ frontend/app/src/main/java/com/aplikasi/checkyourmood/MainActivity.java
@@ -0,0 +1,26 @@
+package com.aplikasi.checkyourmood;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, new MainFragment()).commit();
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.camera);
+    }
+}
